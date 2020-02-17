17149b65e6ea4697a3604c5e8f27442f349d8caf
Kathy Martinez <katherynymartinez@gmail.com>
2020-02-16 22:58:17 -0500

    fixed defaults for logo work

--- src/gologolo/GoLogoLoLogo.js
+++ src/gologolo/GoLogoLoLogo.js
@@ -4,10 +4,11 @@ import {GoLogoLoDefaults} from './GoLogoLoConstants.js'
 export default class GoLogoLoLogo extends AppWork {
     constructor(name) {
         super(name);
-        this.text = GoLogoLoDefaults.GOLOGOLO_DEFAULT_TEXT_COLOR;
+        this.text = GoLogoLoDefaults.GOLOGOLO_DEFAULT_TEXT;
         this.fontSize = GoLogoLoDefaults.GOLOGOLO_DEFAULT_FONT_SIZE;
-        this.textColor = GoLogoLoDefaults.GOLOGOLO_DEFAULT_BACKGROUND_COLOR;
-        this.backgroundColor = GoLogoLoDefaults.GOLOGOLO_DEFAULT_BORDER_COLOR;
+        this.textColor = GoLogoLoDefaults.GOLOGOLO_DEFAULT_TEXT_COLOR;
+        this.backgroundColor = GoLogoLoDefaults.GOLOGOLO_DEFAULT_BACKGROUND_COLOR;
+        this.borderColor = GoLogoLoDefaults.GOLOGOLO_DEFAULT_BORDER_COLOR;
         this.borderRadius = GoLogoLoDefaults.GOLOGOLO_DEFAULT_BORDER_RADIUS;
         this.borderStyle = GoLogoLoDefaults.GOLOGOLO_DEFAULT_BORDER_TYPE;
         this.borderWidth = GoLogoLoDefaults.GOLOGOLO_DEFAULT_BORDER_THICKNESS;
